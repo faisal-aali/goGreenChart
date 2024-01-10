c5989dff381f192c36f28b0c16511ecb22d33cd7
faisal-aali <55294341+faisal-aali@users.noreply.github.com>
2024-01-10 09:44:45 +0500

    Update chart.js

--- chart.js
+++ chart.js
@@ -4673,6 +4673,7 @@
                 // Rename keys
                 const keys = Object.keys(t);
                 let payload = {};
+                    debugger;
                 keys.forEach(key => {
                     switch (key){
                         case 'Regions':
